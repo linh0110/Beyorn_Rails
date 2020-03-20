b96e88233f04871170787a8c31af70a4352145c9
Sam Ruby <rubys@intertwingly.net>
2020-03-20 17:09:40 +0700

    Task F: Add a Dash of Ajax

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -21,7 +21,7 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     follow_redirect!
-    
+
     assert_select "h2", "Your Cart"
     assert_select "td", "Programming Ruby 1.9"
   end
@@ -47,5 +47,16 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to line_items_url
+  end
+
+  test "should create line_item via ajax" do
+    assert_difference("LineItem.count") do
+      post line_items_url, params: { product_id: products(:ruby).id },
+                           xhr: true
+    end
+    assert_response :success
+    assert_select_jquery :html, "#cart" do
+      assert_select "tr#current_item td", /Programming Ruby 1.9/
+    end
   end
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -47,7 +47,6 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_difference("Product.count", -1) do
       delete product_url(@product)
     end
-
     assert_redirected_to products_url
   end
 
@@ -58,10 +57,4 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to products_url
   end
   
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
-    end
-    assert_redirected_to products_url
-  end
 end

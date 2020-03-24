bf16ef6c02d3c9f7beed7add4455949c83cc7a10
linh0110 <lvlthpttt98@gmail.com>
2020-03-24 17:26:38 +0700

    Task J: Internationalization

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,20 @@
 Rails.application.routes.draw do
-  get 'admin' => 'admin#index'
+  get "admin" => "admin#index"
 
   controller :sessions do
-    get  'login' => :new
-    post 'login' => :create
-    delete 'logout' => :destroy
+    get "login" => :new
+    post "login" => :create
+    delete "logout" => :destroy
   end
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  root 'store#index', as: 'store_index'
-
+  scope "(:locale)" do
+    # Ex:- scope :active, -> {where(:active => true)}
+    resources :orders
+    resources :line_items
+    resources :carts
+    root "store#index", as: "store_index", via: :all
+  end
   resources :products do
     get :who_bought, on: :member
   end
@@ -20,4 +22,3 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 end
-
